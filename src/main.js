9fdf9b6d6f0fddab37676e37980a083d5d25ad18
Oleksii_S <lex.sydoruk@gmail.com>
2023-10-31 23:37:21 +0100

    added smooth scroll JS

--- src/main.js
+++ src/main.js
@@ -12,3 +12,37 @@
     nav_menu.menu.classList.toggle('is-open');
   }
 })();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substr(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      const yOffset = targetElement.getBoundingClientRect().top;
+      const initialYOffset = window.pageYOffset;
+      const duration = 1300; // You can adjust the duration as needed
+      const startTime = performance.now();
+
+      function step(currentTime) {
+        const timeElapsed = currentTime - startTime;
+        window.scrollTo(
+          0,
+          easeOut(timeElapsed, initialYOffset, yOffset, duration)
+        );
+        if (timeElapsed < duration) {
+          requestAnimationFrame(step);
+        }
+      }
+
+      function easeOut(t, b, c, d) {
+        t /= d;
+        return -c * t * (t - 2) + b;
+      }
+
+      requestAnimationFrame(step);
+    }
+  });
+});
